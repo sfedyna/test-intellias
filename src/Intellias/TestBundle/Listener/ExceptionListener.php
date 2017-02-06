6522c7fba81577d3184c98e861f6a6c1fcf28898
sfedyna <slfedyna@gmail.com>
2017-02-06 10:18:09 +0200

    Deploy source code on git server

---
+++ src/Intellias/TestBundle/Listener/ExceptionListener.php
@@ -0,0 +1,41 @@
+<?php
+namespace Intellias\TestBundle\Listener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+
+class ExceptionListener
+{
+    protected $templating;
+    protected $kernel;
+
+    public function __construct(EngineInterface $templating, $kernel)
+    {
+        $this->templating = $templating;
+        $this->kernel = $kernel;
+    }
+
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        if ('prod' == $this->kernel->getEnvironment()){
+            $exception = $event->getException();
+            $response = new Response();
+            $response->setContent(
+                $this->templating->render(
+                    '::exception.html.twig',
+                    array('exception' => $exception)
+                )
+            );
+            if ($exception instanceof HttpExceptionInterface) {
+                $response->setStatusCode($exception->getStatusCode());
+                $response->headers->replace($exception->getHeaders());
+            } else {
+                $response->setStatusCode(500);
+            }
+            $event->setResponse($response);
+        }
+    }
+} 
